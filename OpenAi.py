d98bd61d18ae82e671cf6081a752fe91a49900dd
shami <alshamicourses@gmail.com>
2023-01-08 18:22:26 +0800

    2nd commit

--- OpenAi.py
+++ OpenAi.py
@@ -1,36 +1,24 @@
 from private.bot_tokens import chatgpt_token
 import openai
+import discord
 openai.api_key = chatgpt_token
-
-def debug(code, language):
-
-
-    response = openai.Completion.create(
-      model="code-davinci-002",
-      prompt=f"""##### Fix bugs in the below function### Buggy {language}\n{code}\n### Fixed {language}""",
-      temperature=0,
-      max_tokens=182,
-      top_p=1.0,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-      stop=["###"]
-    )
-    output =response["choices"][0]["text"]
-    output = output.strip()
-    return output
+preConfig =""
+# """
+# This is information about you and if you receive any question related to this information, you can refer to it "I'm a bot called Aven. I was created and developed by shami. wanna know about my creator you can visit his github account github/MohammedAlshami".\n
+# """
 
 def write_code(code="simple code", language = "python"):
+    global preConfig
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"""{code} ###write this using {language}\n""",
+        prompt=f"{preConfig}{code}\n",
         temperature=0,
-        top_p=1.0,
-        frequency_penalty=0.5,
-        presence_penalty=0.5,
-        max_tokens=2048)
+        max_tokens=2048,
+        top_p=1,
+        frequency_penalty=0.2,
+        presence_penalty=0
+    )
+    embedVar = discord.Embed(title="OUTPUT", description=f"""```{response["choices"][0]["text"]}```""", color=0x00ff00)
+    return embedVar
 
-    return f"""{response["choices"][0]["text"]}"""
 
-code = """
-write a python list that contains multiple essays on why I should be the president 
-"""
